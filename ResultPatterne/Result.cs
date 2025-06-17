c82a3be62c011348497b00c72b584e848e4c11d3
egor2 <egor2@DESKTOP-1ICV58B>
2025-06-17 23:50:30 +0300

    Add project files.

---
+++ ResultPatterne/Result.cs
@@ -0,0 +1,24 @@
+﻿public class Result
+{
+    public string? ResponseText { get; }
+
+    public string? Error { get; }
+
+    public int StatusCode { get; }
+
+    public bool IsSuccess { get; }
+
+    private Result(bool isSuccess, string error, int statusCode, string? reponseText = null)
+    {
+        ResponseText = reponseText;
+        StatusCode = statusCode;
+        Error = error;
+        IsSuccess = isSuccess;
+    }
+
+    public static Result Success(int statusCode, string? responseText = null)
+        => new(true, null!, statusCode, responseText);
+
+    public static Result Failure(string error, int statusCode)
+        => new(false, error, statusCode);
+}
---
+++ ResultPatterne/ResultExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Results
+{
+    public static class ResultExtensions
+    {
+        public static IActionResult ToActionResult(this Result result)
+        {
+            return result.IsSuccess
+                ? new ObjectResult(new { Message = result.ResponseText }) { StatusCode = result.StatusCode }
+                : new ObjectResult(new { Message = result.Error }) { StatusCode = result.StatusCode };
+        }
+
+        public static IActionResult ToActionResult<TValue, TError>(this Result<TValue, TError> result)
+        {
+            return result.IsSuccess
+                ? new ObjectResult(result.Value) { StatusCode = result.StatusCode }
+                : new ObjectResult(new { Message = result.Error }) { StatusCode = result.StatusCode };
+        }
+    }
+}
